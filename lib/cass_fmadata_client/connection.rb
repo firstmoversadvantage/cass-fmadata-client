fa4106361c22558e3ca50ec98b169bac85c1caa3
pawjer <pawel.jermalonek@polcode.net>
2019-01-22 15:14:22 +0100

    Add retry_logic settings to initilizer

--- lib/cass_fmadata_client/connection.rb
+++ lib/cass_fmadata_client/connection.rb
@@ -6,10 +6,15 @@ module CassClient
 
     DEFAULT_URL     = 'www.example.com'.freeze
     DEFAULT_VERSION = '/api'.freeze
+    # retry logic defaults:
+    DEFAULT_MAX_RETRIES = 5
+    DEFAULT_RETRY_INTERVAL = 5
 
     def setup_connection(p = {})
-      @host  = p[:host] || DEFAULT_URL
-      @token = p[:token]
+      @host           = p[:host]            || DEFAULT_URL
+      @token          = p[:token]
+      @max_retries    = p[:max_retries]     || DEFAULT_MAX_RETRIES
+      @retry_interval = p[:retry_interval]  || DEFAULT_RETRY_INTERVAL
     end
 
     def headers
