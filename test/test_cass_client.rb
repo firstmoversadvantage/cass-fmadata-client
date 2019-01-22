3a6146f35eba5393f8ef4b6e473f34120060847a
pawjer <pawel.jermalonek@polcode.net>
2019-01-22 15:12:05 +0100

    Prepare tests for retry logic

--- test/test_cass_client.rb
+++ test/test_cass_client.rb
@@ -119,4 +119,55 @@ class CassClientTest < Minitest::Test
     response = client.address(params)
     assert_equal response.code, '200'
   end
+
+  def test_retry_logic_service_unavailable
+    client = CassFma::Client.new(host:  ENV['CASS_URL'],
+                                 token: ENV['CASS_TOKEN'],
+                                 max_retries: 3,
+                                 retry_interval: 1)
+
+    params =  { "city": "Boulder",
+                "state": "CO",
+                "street_address": "2950 N Lakeridge Trl",
+                "zip": "80302" }
+
+    stub_request(:get, ENV['CASS_URL'] + 'address/?city=Boulder&state=CO&street_address=2950+N+Lakeridge+Trl&zip=80302').
+      with(headers: {'Accept':          '*/*',
+                     'Accept-Encoding': 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+                     'Token':           ENV['CASS_TOKEN'],
+                     'User-Agent':      'Ruby'}).
+      to_return(status: 503, body: "", headers: {})
+
+    error = assert_raises CassFma::BadResponseError do
+      client.address(params)
+    end
+
+    assert_equal 'Net::HTTPServiceUnavailable (MAX_RETRIES reached)', error.message
+  end
+
+
+  def test_retry_logic_bat_gateway
+    client = CassFma::Client.new(host:  ENV['CASS_URL'],
+                                 token: ENV['CASS_TOKEN'],
+                                 max_retries: 3,
+                                 retry_interval: 1)
+
+    params =  { "city": "Boulder",
+                "state": "CO",
+                "street_address": "2950 N Lakeridge Trl",
+                "zip": "80302" }
+
+    stub_request(:get, ENV['CASS_URL'] + 'address/?city=Boulder&state=CO&street_address=2950+N+Lakeridge+Trl&zip=80302').
+      with(headers: {'Accept':          '*/*',
+                     'Accept-Encoding': 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+                     'Token':           ENV['CASS_TOKEN'],
+                     'User-Agent':      'Ruby'}).
+      to_return(status: 502, body: "Bad Gateway", headers: {})
+
+    error = assert_raises CassFma::BadResponseError do
+      client.address(params)
+    end
+
+    assert_equal 'Net::HTTPBadGateway (MAX_RETRIES reached)', error.message
+  end
 end
